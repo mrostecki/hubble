86e988fb8f3f1b5f3d03e65a1a741c57b5f2e7a9
Sebastian Wicki <sebastian@isovalent.com>
2019-11-20 11:58:07 +0100

    parser: Use protobuf definitions from "flow" package
    Signed-off-by: Sebastian Wicki <sebastian@isovalent.com>

--- pkg/parser/seven/parser_test.go
+++ pkg/parser/seven/parser_test.go
@@ -27,7 +27,7 @@ import (
 	"github.com/cilium/cilium/pkg/monitor/api"
 	"github.com/cilium/cilium/pkg/proxy/accesslog"
 	"github.com/cilium/cilium/pkg/u8proto"
-	pb "github.com/cilium/hubble/api/v1/observer"
+	pb "github.com/cilium/hubble/api/v1/flow"
 	"github.com/cilium/hubble/pkg/testutils"
 
 	"github.com/gogo/protobuf/types"
